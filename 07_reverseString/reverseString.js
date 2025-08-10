830162af3a1f4afaa6024a6908bf2f087d7a1bad
Nicolás Di virgilio <nicolas_dv93@outlook.com>
2025-08-10 11:06:27 -0300

    reverse

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
 
+const reverseString = function(str) {
+let palabra= str.split("").reverse().join("")
+return palabra
 };
-
+let palabras=reverseString("hello there")
+console.log(palabras);
 // Do not edit below this line
 module.exports = reverseString;
