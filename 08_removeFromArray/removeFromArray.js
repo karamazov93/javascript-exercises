c59178ff5f4e0d243be294efff65f1d1ed4fa948
Nicolás Di virgilio <nicolas_dv93@outlook.com>
2025-08-10 13:25:29 -0300

    remove from other matriz using rest parameters

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+
+function removeFromArray(array, ...lista){
+    let newArrays=[]
+    array.forEach(item => {
+        if(!lista.includes(item)){
+            newArrays.push(item)
+        }
+    });
+return newArrays
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
